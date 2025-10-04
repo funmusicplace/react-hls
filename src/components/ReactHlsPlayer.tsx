4b451bf4c5a5fd25d3b912b664d5ac292909e4c0
Simon <simon@mirlo.space>
2025-10-04 08:27:41 -0400

    chore: types

--- src/components/ReactHlsPlayer.tsx
+++ src/components/ReactHlsPlayer.tsx
@@ -1,114 +1,117 @@
-import React, { useEffect, RefObject } from 'react'
-import Hls, { HlsConfig } from 'hls.js'
+import React, { useEffect, RefObject } from 'react';
+import Hls, { HlsConfig, ErrorData } from 'hls.js';
 
 declare global {
-    interface Window {
-        Hls: typeof Hls
-    }
+  interface Window {
+    Hls: typeof Hls;
+  }
 }
 
-export interface ReactHlsPlayerProps extends React.VideoHTMLAttributes<HTMLVideoElement> {
-    hlsConfig?: Partial<HlsConfig>
-    playerRef?: RefObject<HTMLVideoElement>
-    getHLSInstance?: (hls: Hls) => void
-    src: string
+export interface ReactHlsPlayerProps
+  extends React.VideoHTMLAttributes<HTMLVideoElement> {
+  hlsConfig?: Partial<HlsConfig>;
+  playerRef?: RefObject<HTMLVideoElement>;
+  getHLSInstance?: (hls: Hls) => void;
+  src: string;
+  onError?: (event: unknown, data?: ErrorData) => void;
 }
 
 const ReactHlsPlayer: React.FC<ReactHlsPlayerProps> = ({
-    hlsConfig,
-    playerRef,
-    getHLSInstance,
-    src,
-    autoPlay,
-    ...props
+  hlsConfig,
+  playerRef,
+  getHLSInstance,
+  src,
+  autoPlay,
+  onError,
+  ...props
 }) => {
-    const internalRef = playerRef || React.useRef<HTMLVideoElement>(null)
+  const internalRef = playerRef || React.useRef<HTMLVideoElement>(null);
 
-    useEffect(() => {
-        let hls: Hls | null
+  useEffect(() => {
+    let hls: Hls | null;
 
-        function initPlayer() {
-            if (hls != null) {
-                hls.destroy()
-            }
+    function initPlayer() {
+      if (hls != null) {
+        hls.destroy();
+      }
 
-            const newHls = new Hls({
-                enableWorker: false,
-                ...hlsConfig,
-            })
+      const newHls = new Hls({
+        enableWorker: false,
+        ...hlsConfig,
+      });
 
-            newHls.on(Hls.Events.MEDIA_ATTACHED, () => {
-                newHls.loadSource(src)
-            })
+      newHls.on(Hls.Events.MEDIA_ATTACHED, () => {
+        newHls.loadSource(src);
+      });
 
-            newHls.on(Hls.Events.MANIFEST_PARSED, () => {
-                if (!autoPlay) {
-                    return
-                }
-
-                try {
-                    internalRef?.current?.play()
-                } catch (error) {
-                    console.warn('Play is not supported in this environment', error)
-                }
-            })
-
-            newHls.on(Hls.Events.ERROR, (event, data) => {
-                props.onError && props.onError(event, data);
-
-                if (!data.fatal) {
-                    return
-                }
-                console.warn(`HLS.js Error: ${data.type} - ${data.details}`)
-                switch (data.type) {
-                    case Hls.ErrorTypes.NETWORK_ERROR:
-                        newHls.startLoad()
-                        break
-                    case Hls.ErrorTypes.MEDIA_ERROR:
-                        newHls.recoverMediaError()
-                        break
-                    default:
-                        initPlayer()
-                        break
-                }
-            })
-
-            if (internalRef.current) {
-                newHls.attachMedia(internalRef.current)
-            }
-
-            hls = newHls
-            getHLSInstance?.(newHls)
+      newHls.on(Hls.Events.MANIFEST_PARSED, () => {
+        if (!autoPlay) {
+          return;
         }
 
-        if (Hls.isSupported()) {
-            initPlayer()
-        } else {
-            console.warn('HLS is not supported in this browser')
+        try {
+          internalRef?.current?.play();
+        } catch (error) {
+          console.warn('Play is not supported in this environment', error);
         }
+      });
 
-        return () => {
-            if (hls != null) {
-                hls.destroy()
-            }
+      newHls.on(Hls.Events.ERROR, (event, data) => {
+        onError && onError(event, data);
+
+        if (!data.fatal) {
+          return;
         }
-    }, [autoPlay, hlsConfig, playerRef, getHLSInstance, src])
+        console.warn(`HLS.js Error: ${data.type} - ${data.details}`);
+        switch (data.type) {
+          case Hls.ErrorTypes.NETWORK_ERROR:
+            newHls.startLoad();
+            break;
+          case Hls.ErrorTypes.MEDIA_ERROR:
+            newHls.recoverMediaError();
+            break;
+          default:
+            initPlayer();
+            break;
+        }
+      });
 
-    if (!Hls.isSupported()) {
-        return (
-            <video
-                ref={internalRef}
-                src={src}
-                autoPlay={autoPlay}
-                data-testid="react-hls-player-fallback"
-                {...props}
-            >
-                Your browser does not support the video tag.
-            </video>
-        )
+      if (internalRef.current) {
+        newHls.attachMedia(internalRef.current);
+      }
+
+      hls = newHls;
+      getHLSInstance?.(newHls);
     }
 
-    return <video ref={internalRef} data-testid="react-hls-player" {...props} />
-}
+    if (Hls.isSupported()) {
+      initPlayer();
+    } else {
+      console.warn('HLS is not supported in this browser');
+    }
 
-export default ReactHlsPlayer
+    return () => {
+      if (hls != null) {
+        hls.destroy();
+      }
+    };
+  }, [autoPlay, hlsConfig, playerRef, getHLSInstance, src]);
+
+  if (!Hls.isSupported()) {
+    return (
+      <video
+        ref={internalRef}
+        src={src}
+        autoPlay={autoPlay}
+        data-testid="react-hls-player-fallback"
+        {...props}
+      >
+        Your browser does not support the video tag.
+      </video>
+    );
+  }
+
+  return <video ref={internalRef} data-testid="react-hls-player" {...props} />;
+};
+
+export default ReactHlsPlayer;
